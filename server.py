f898f7a5cb9e0c18313db5e936fe33a20db48883
griffin-brown-95 <154379139+griffin-brown-95@users.noreply.github.com>
2024-05-30 00:02:58 -0600

    updated requirements

--- server.py
+++ server.py
@@ -1,7 +1,5 @@
 from flask import Flask, request, render_template
 from movie_selector import get_recommendations, movie_titles
-from waitress import serve
-import os
 
 app = Flask(__name__)
 
@@ -14,9 +12,10 @@ def home():
         recommendations = get_recommendations(movie_title)
     return render_template('home.html', movie_titles=movie_titles, movie_title=movie_title, recommendations=recommendations)
 
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8000, debug=True)
+
+
 """ if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8000))
     serve(app, host="0.0.0.0", port=port) """
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8000, debug=True)
