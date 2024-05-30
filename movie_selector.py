0051f04f4b69b0b49aa599fd75f4842bff399ec5
griffin-brown-95 <154379139+griffin-brown-95@users.noreply.github.com>
2024-05-30 00:56:02 -0600

    production commit

--- movie_selector.py
+++ movie_selector.py
@@ -1,36 +1,36 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
-from flask import Flask, request, render_template
 from sklearn.preprocessing import MinMaxScaler
-
-app = Flask(__name__)
+import numpy as np
 
 # Load the data
 df_url = 'https://storage.googleapis.com/data-projects/top_1000_popular_movies_tmdb.csv'
 df = pd.read_csv(df_url, lineterminator='\n')
-df.to_csv('example.csv')
 df = df.drop(df.columns[0], axis=1)
 
+# Compute the TF-IDF matrix and cosine similarity matrix
 tfidf = TfidfVectorizer(stop_words='english')
 df['overview'] = df['overview'].fillna('')
 tfidf_matrix = tfidf.fit_transform(df['overview'])
 cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
 
+# Normalize the vote_average, vote_count, and popularity columns
 scaler = MinMaxScaler()
 df[['vote_average', 'vote_count', 'popularity']] = scaler.fit_transform(df[['vote_average', 'vote_count', 'popularity']])
 
-def get_weighted_similarity(idx, 
-                            cosine_sim=cosine_sim, 
-                            df=df, 
-                            weight_votes=0.02, 
-                            weight_popularity=0.02):
+# Calculate time decay
+df['release_date'] = pd.to_datetime(df['release_date'], errors='coerce')
+current_date = pd.to_datetime('today')
+df['days_since_release'] = (current_date - df['release_date']).dt.days
+df['time_decay'] = np.exp(-df['days_since_release'] / 365)
+
+def get_weighted_similarity(idx, cosine_sim=cosine_sim, df=df, weight_votes=0.02, weight_popularity=0.02):
     sim_scores = list(enumerate(cosine_sim[idx]))
     vote_avg_scores = df['vote_average'].values
     vote_count_scores = df['vote_count'].values
     popularity_scores = df['popularity'].values
     
-    # Calculate the weighted score
     weighted_scores = []
     for i, score in sim_scores:
         weighted_score = score + \
@@ -41,32 +41,30 @@ def get_weighted_similarity(idx,
     
     return weighted_scores
 
-def get_recommendations(title, 
-                        cosine_sim=cosine_sim, 
-                        df=df, 
-                        weight_votes=0.05, 
-                        weight_popularity=0.05):
-    idx = df[df['title'] == title].index[0]
-    # adding in similarity by scores
-    sim_scores = get_weighted_similarity(idx, 
-                                         cosine_sim, 
-                                         df, 
-                                         weight_votes, 
-                                         weight_popularity,)
+def get_recommendations_table(title, cosine_sim=cosine_sim, df=df, weight_votes=0.05, weight_popularity=0.05):
+    try:
+        idx = df[df['title'].str.contains(title, case=False, na=False)].index[0]
+    except IndexError:
+        return "<p>No recommendations found for the given title.</p>"
+    
+    print(f"Index of the movie '{title}': {idx}")
+    
+    sim_scores = get_weighted_similarity(idx, cosine_sim, df, weight_votes, weight_popularity)
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
     sim_scores = sim_scores[1:11]
     movie_indices = [i[0] for i in sim_scores]
-    return df['title'].iloc[movie_indices]
+    
+    #print(f"Movie indices for recommendations: {movie_indices}")
+    #print(f"Similarity scores: {sim_scores}")
+    
+    recommendations = df['title'].iloc[movie_indices]
+    recommendations_df = recommendations.to_frame(name='Title')
+    html_table = recommendations_df.to_html(classes='table table-striped', index=False)
+    html_table = html_table.replace('<th>Title</th>', '<th style="text-align: left;">Title</th>')
+    
+    return html_table
 
-@app.route('/')
-def home():
-    return render_template('home.html')
 
-@app.route('/recommend', methods=['POST'])
-def recommend():
-    movie_title = request.form['title']
-    recommendations = get_recommendations(movie_title)
-    return render_template('result.html', movie_title=movie_title, recommendations=recommendations)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#html_table = get_recommendations_table("Fast X")
+#print(html_table)
--- server.py
+++ server.py
@@ -1,21 +1,22 @@
 from flask import Flask, request, render_template
-from movie_selector import get_recommendations, movie_titles
+from movie_selector import get_recommendations_table
+from waitress import serve
+import os
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    recommendations = None
+    recommendations_table = None
     movie_title = None
     if request.method == 'POST':
         movie_title = request.form['title']
-        recommendations = get_recommendations(movie_title)
-    return render_template('home.html', movie_titles=movie_titles, movie_title=movie_title, recommendations=recommendations)
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8000, debug=True)
-
+        recommendations_table = get_recommendations_table(movie_title)
+    return render_template('home.html', movie_title=movie_title, recommendations_table=recommendations_table)
 
 """ if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8000, debug=True) """
+
+if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8000))
-    serve(app, host="0.0.0.0", port=port) """
+    serve(app, host="0.0.0.0", port=port)
